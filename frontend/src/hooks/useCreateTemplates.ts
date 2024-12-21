1816af43c2dbd49851e382ae495e8747954a6a2f
Maksheev, Ilya (Quantori LLC) <ilya.maksheev@astrazeneca.com>
2024-12-21 17:26:44 +0500

    feat(createTemplates): add reset atom

--- frontend/src/hooks/useCreateTemplates.ts
+++ frontend/src/hooks/useCreateTemplates.ts
@@ -1,27 +1,36 @@
 import { useAtom } from "jotai"
 import { useCreateCardsTemplate } from "../api/cardsTemplates"
 import { createTemplateAtom } from "../state/atoms"
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { RESET } from "jotai/utils"
 
 export const useCreateTemplate = () => {
   const createCardsTemplate = useCreateCardsTemplate()
 
-  const [ template, setTemplate ] = useAtom(createTemplateAtom)
+  const [template, setTemplate] = useAtom(createTemplateAtom)
 
   const addTemplateField = () => {
-    setTemplate([...template, {title: "", subtitle: ""}])
+    setTemplate([...template, { title: "", subtitle: "" }])
   }
 
   const removeTemplateStructure = (removeIndex: number) => {
-    setTemplate(template.filter(( _, index ) => (index !== removeIndex)))
+    setTemplate(template.filter((_, index) => (index !== removeIndex)))
   }
 
   const changeTemplateStructure = (index: number, name: string, value: string) => {
-    setTemplate(template.map((item, i) => i === index ? {...item, [name]: value} : item))
+    setTemplate(template.map((item, i) => i === index ? { ...item, [name]: value } : item))
   }
+
+  const queryClient = useQueryClient()
 
   const createCardsTemplateMutation = useMutation({
     mutationFn: createCardsTemplate,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: "templates" })
+    },
+    onSettled: () => {
+      setTemplate(RESET)
+    }
   })
 
   return {
--- frontend/src/state/atoms.ts
+++ frontend/src/state/atoms.ts
@@ -1,8 +1,8 @@
 import { atom } from "jotai";
-import { atomWithStorage } from "jotai/utils";
+import { atomWithReset, atomWithStorage } from "jotai/utils";
 import { TemplateStructure } from "../types/cardTemplate";
 
 export const userAtom = atom<{ username: string } | null>(null)
 export const tokenAtom = atomWithStorage<string | null>("accessToken", null)
 
-export const createTemplateAtom = atom<TemplateStructure[]>([{title: "", subtitle: ""}])
+export const createTemplateAtom = atomWithReset<TemplateStructure[]>([{title: "", subtitle: ""}])
