7859e9df0732485ef3fa9f3c01cf407b5e1b18b7
KorvinEC <maksheev777@mail.ru>
2024-12-26 13:54:40 +0500

    feat(backend): format database models

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Column, String, Integer, JSON, ForeignKey
+from sqlalchemy import JSON, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy.sql import text
@@ -11,49 +11,58 @@ from database.session import Base
 class Users(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
     username: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(nullable=False)
-    nickname: Mapped[str]
+    nickname: Mapped[str] = mapped_column()
 
-    templates: Mapped[list["Templates"]] = relationship("Templates", back_populates="user",
-                                                        cascade="all, delete-orphan")
-    cards: Mapped[list["Cards"]] = relationship("Cards", back_populates="user",
-                                                        cascade="all, delete-orphan")
+    templates: Mapped[list["Templates"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+    cards: Mapped[list["Cards"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
 
 class Templates(Base):
     __tablename__ = "templates"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    structure: Mapped[dict[str, str]] = Column(JSON, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    structure: Mapped[dict[str, str]] = mapped_column(JSON, nullable=False)
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
-    user: Mapped[Users] = relationship("Users", back_populates="templates")
+    user: Mapped[Users] = relationship(back_populates="templates")
 
-    cards: Mapped[list["Cards"]] = relationship("Cards", back_populates="template", cascade="all, delete-orphan")
+    cards: Mapped[list["Cards"]] = relationship(
+        back_populates="template", cascade="all, delete-orphan"
+    )
 
 
 class Cards(Base):
     __tablename__ = "cards"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    data: Mapped[list[dict[str, str]]] = Column(JSON, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    data: Mapped[list[dict[str, str]]] = mapped_column(JSON, nullable=False)
 
     template_id: Mapped[int] = mapped_column(ForeignKey("templates.id"), nullable=False)
-    template: Mapped[Templates] = relationship("Templates", back_populates="cards")
+    template: Mapped[Templates] = relationship(back_populates="cards")
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
-    user: Mapped[Users] = relationship("Users", back_populates="cards")
+    user: Mapped[Users] = relationship(back_populates="cards")
 
-    images: Mapped[list["Images"]] = relationship("Images", back_populates="card", cascade="all, delete-orphan")
+    images: Mapped[list["Images"]] = relationship(
+        back_populates="card", cascade="all, delete-orphan"
+    )
 
 
 class Images(Base):
     __tablename__ = "images"
 
-    id: Mapped[uuid.UUID] = Column(UUID(as_uuid=True), primary_key=True, server_default=text("(gen_random_uuid())"))
-    path: Mapped[str] = Column(String, nullable=False)
-    url: Mapped[str] = Column(String, nullable=False)
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, server_default=text("(gen_random_uuid())")
+    )
+    path: Mapped[str] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
 
-    card_id: Mapped[int] = Column(ForeignKey("cards.id"), nullable=False)
-    card: Mapped[Cards] = relationship("Cards", back_populates="images")
+    card_id: Mapped[int] = mapped_column(ForeignKey("cards.id"), nullable=False)
+    card: Mapped[Cards] = relationship(back_populates="images")
