22af06fc515fee579cc85c2257c73b504c569a40
KorvinEC <maksheev777@mail.ru>
2024-12-27 13:04:59 +0500

    style(backend): format templates routes

--- backend/app/cards/templates_routes.py
+++ backend/app/cards/templates_routes.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from typing import Annotated, Type
+from typing import Annotated
 
 from fastapi import Depends, APIRouter, status
 from fastapi.responses import Response
@@ -21,20 +21,20 @@ templates_router = router = APIRouter()
 
 @router.get("/", response_model=list[CardTemplate])
 async def get_cards_templates(
-        db: Annotated[Session, Depends(get_db)],
-) -> list[Type[Templates]]:
+    db: Annotated[Session, Depends(get_db)],
+) -> list[Templates]:
     return db.query(Templates).order_by(Templates.id.desc()).all()
 
 
 @router.post("/", response_model=CardTemplate)
 async def create_card_template(
-        input_data: CardTemplateTitles,
-        db: Annotated[Session, Depends(get_db)],
-        current_user: Annotated[User, Depends(get_current_user)],
-) -> CardTemplate:
+    input_data: CardTemplateTitles,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[User, Depends(get_current_user)],
+) -> Templates:
     template = Templates(
         structure=json.dumps(input_data.model_dump().get("structure", [])),
-        user_id=current_user.id
+        user_id=current_user.id,
     )
 
     db.add(template)
@@ -45,17 +45,17 @@ async def create_card_template(
 
 @router.get("/{template_id}", response_model=CardTemplate)
 async def get_card_template(
-        template_id: int,
-        db: Annotated[Session, Depends(get_db)],
-) -> Type[Templates]:
+    template_id: int,
+    db: Annotated[Session, Depends(get_db)],
+) -> Templates:
     return get_card_template_by_id(template_id, db)
 
 
 @router.delete("/{template_id}")
 async def delete_template(
-        template_id: int,
-        db: Annotated[Session, Depends(get_db)],
-        current_user: Annotated[User, Depends(get_current_user)],
+    template_id: int,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[User, Depends(get_current_user)],
 ) -> Response:
     template = get_card_template_by_id(template_id, db)
 
